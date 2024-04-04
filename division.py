a632164628e4321236560b1659731288fe1de1c6
jpdeabreux64 <105162265+jpdeabreux64@users.noreply.github.com>
2024-04-04 19:55:27 -0400

    commit Agrerue la funcion division

---
+++ division.py
@@ -0,0 +1,13 @@
+def dividir(nun1, nun2):
+    if nun2!= 0:
+       return nun1 / nun2
+    else:
+       return" Error divisio{on por cero}"
+ 
+    
+    
+print("4. Dividir: ")    
+nun1=   float(input("Ingrese el primer numero: "))
+nun2=   float(input("Ingrese el segundo numero: "))
+
+print("Resultado: ", dividir(nun1, nun2)) 
