3aed0852d2f761e50a9b8a27fbff7776b93667c5
jpdeabreux64 <105162265+jpdeabreux64@users.noreply.github.com>
2024-10-02 19:29:48 -0400

    Modificacion de mutiplicación

--- multiplicacion.py
+++ multiplicacion.py
@@ -1,7 +1,7 @@
 def multiplicar(nun1, nun2):
     return nun1 * nun2
     
-print("Multiplicación:")
+print("Multiplicacines:")
     
 nun1=   float(input("Ingrese el primer numero: "))
 nun2=   float(input("Ingrese el segundo numero: "))
